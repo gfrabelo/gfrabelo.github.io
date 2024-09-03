5c497fc5f59f03248eaf2da46e5d09df76bc1f5e
gfrabelo <gabrielrabelob@gmail.com>
2024-09-03 14:57:50 -0300

    feat: about tradution

--- js/language.js
+++ js/language.js
@@ -7,7 +7,11 @@ const subTitle = document.querySelector('.bottom-line');
 const aboutNav = document.querySelector('.about');
 const skillsNav = document.querySelector('.skills');
 const contactNav = document.querySelector('.contact');
-const aboutTxt = document.querySelector('.about-text');
+const aboutTitle = document.querySelector('.about-title');
+const aboutTxtP1 = document.querySelector('.about-text-p1');
+const aboutTxtP2 = document.querySelector('.about-text-p2');
+const aboutTxtP3 = document.querySelector('.about-text-p3');
+const skillsTitle = document.querySelector('.skill-title');
 
 // Chance Language on Click
 buttons.forEach( button => {
@@ -22,7 +26,11 @@ buttons.forEach( button => {
         aboutNav.textContent = data[attr].about;
         skillsNav.textContent = data[attr].skills;
         contactNav.textContent = data[attr].contact;
-        // aboutTxt.textContent = data[attr].aboutTxt;
+        aboutTitle.textContent = data[attr].aboutTitle;
+        aboutTxtP1.textContent = data[attr].aboutTxt1;
+        aboutTxtP2.textContent = data[attr].aboutTxt2;
+        aboutTxtP3.textContent = data[attr].aboutTxt3;
+        skillsTitle.textContent = data[attr].skillsTitle;
     });
 });
 
@@ -34,7 +42,11 @@ const data = {
         'about': 'Sobre',
         'skills': 'Habilidades',
         'contact': 'Contato',
-        'aboutTxt': 'Olá, meu nome é Gabriel Rabelo, moro em São Paulo e há 7 anos me aventuro no incrível mundo da programação! Apaixonado desde a infância pelas infinitas possibilidades de criação que a tecnologia oferece. Sou fascinado por tornar a ciência e o conhecimento mais acessíveis, contribuindo para um mundo melhor. Tive a oportunidade de trabalhar em projetos para empresas renomadas como Nike, Jequiti e Crackle. Além da minha experiência corporativa, também atuei como desenvolvedor freelancer, onde criei Landing Pages e Sites com altas taxas de conversão. Estou sempre buscando aprender mais, pois o mundo da tecnologia pode nos trazer infinitas possibilidades de criação. Me divirto fazendo isso e busco construir softwares que ofereçam ótimas experiências para meus futuros clientes!'
+        'aboutTitle': 'Sobre Mim',
+        'aboutTxt1': 'Olá, meu nome é Gabriel Rabelo, moro em São Paulo e há 7 anos me aventuro no incrível mundo da programação! Apaixonado desde a infância pelas infinitas possibilidades de criação que a tecnologia oferece. Sou fascinado por tornar a ciência e o conhecimento mais acessíveis, contribuindo para um mundo melhor.',
+        'aboutTxt2': 'Tive a oportunidade de trabalhar em projetos para empresas renomadas como Nike, Jequiti e Crackle. Além da minha experiência corporativa, também atuei como desenvolvedor freelancer, onde criei Landing Pages e Sites com altas taxas de conversão.',
+        'aboutTxt3': 'Estou sempre buscando aprender mais, pois o mundo da tecnologia pode nos trazer infinitas possibilidades de criação. Me divirto fazendo isso e busco construir softwares que ofereçam ótimas experiências para meus futuros clientes!',
+        'skillsTitle': 'Habilidades'
     },
     'english': {
         'title': 'Web Developer',
@@ -42,6 +54,10 @@ const data = {
         'about': 'About',
         'skills': 'Skills',
         'contact': 'Contact',
-        'aboutTxt': 'Hello, my name is Gabriel Rabelo, I live in São Paulo, and I have been exploring the amazing world of programming for 7 years! I have been passionate about the endless possibilities that technology offers since childhood. I am fascinated by making science and knowledge more accessible, contributing to a better world. I’ve had the opportunity to work on projects for renowned companies like Nike, Jequiti, and Crackle. In addition to my corporate experience, I have also worked as a freelance developer, creating Landing Pages and Websites with high conversion rates. I am always eager to learn more because the world of technology offers infinite possibilities for creation. I have fun doing it and strive to build software that provides great experiences for my future clients!'
+        'aboutTitle': 'About Me',
+        'aboutTxt1': 'Hello, my name is Gabriel Rabelo, I live in São Paulo, and I have been exploring the amazing world of programming for 7 years! I have been passionate about the endless possibilities that technology offers since childhood. I am fascinated by making science and knowledge more accessible, contributing to a better world.',
+        'aboutTxt2':'I’ve had the opportunity to work on projects for renowned companies like Nike, Jequiti, and Crackle. In addition to my corporate experience, I have also worked as a freelance developer, creating Landing Pages and Websites with high conversion rates.',
+        'aboutTxt3':'I am always eager to learn more because the world of technology offers infinite possibilities for creation. I have fun doing it and strive to build software that provides great experiences for my future clients!',
+        'skillsTitle': 'Skills'
     }
 }
